20ac97e0e96510fe81c2fb104c82af1adae332e0
Melroy Dsouza <melroydsouza176@gmail.com>
2020-09-26 18:34:33 -0700

    initial push

---
+++ backend/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, send_from_directory
+import os
+
+# os.chdir("../ui/AngularTest")
+# print(os.getcwd())
+# os.system("ng build --outputPath=../../backend/static")
+
+
+app = Flask(__name__, static_url_path='/static')
+
+
+@app.route('/test')
+def test_route():
+    return request.args.get('user')
+
+@app.route('/', defaults={'path': '/static'})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
+
+if __name__ == "__main__":
+    app.run(port=8080)
